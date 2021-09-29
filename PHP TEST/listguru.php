9f66273c97358c3c649c0e61765fb87fc49afae2
rekhzz <rakhadroid2015.com>
2021-09-29 10:36:27 +0700

    revisi tampilan status daftar

--- PHP TEST/listguru.php
+++ PHP TEST/listguru.php
@@ -31,52 +31,55 @@
     </header>
   </div>
 </head>
-<body>
-    <h1 align="center">Daftar Informasi Guru</h1>
-    
-         
+<body>         
     <br>
     <br>
-    <table class = "tabel" align="center" width="1200px" border="1" cellpadding="10" cellspacing="0">
-    
-    <tr>
-        <th>
-            <a href = "create_guru.php"><button class="btn btn-primary me-md-2" type="button">Tambah Data Guru</button></a>
-        </th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th><form  action="" method="post">
-            <input type="text" name="keyword_guru" placeholder="Ketik Nama" autocomplete="off"> 
-            <button class="btn btn-primary me-md-2" type="submit" name="cari_guru">Cari</button>
-            </form>
-        </th>
-
-    </tr>
-        <tr>
-            <th>Aksi</th>
-            <th>NIP</th>
-            <th>Nama Lengkap</th>
-            <th>Tanggal Lahir</th>
-            <th>Jenis Kelamin</th>
-            <th>Alamat</th>
-        </tr>
-    <?php foreach ( $guru as $show ):  ?>
-        <tr>
-            <td>
-                <a href="update_guru.php?id_guru=<?= $show ["NIP"]; ?>"><button type="button" class="btn btn-success">Ubah</button></a> |
-                <a href="delete_guru.php?id_guru=<?= $show ["NIP"]; ?>" onclick="return confirm('Yakin Menghapus Data ?');"><button type="button" class="btn btn-danger">Hapus</button></a>
-            </td>
-            <td> <?= $show ["NIP"] ?> </td>
-            <td> <?= $show ["nama"] ?> </td>
-            <td> <?= $show ["tanggal_lahir"] ?> </td>
-            <td> <?= $show ["jenis_kelamin"] ?></td>
-            <td> <?= $show["alamat"] ?></td>
-        </tr>
-    <?php endforeach ?>
-
-    </table>
+<div class="container">
+    <div class="card mt-3">
+                    <div class="card-header bg-primary text-white">
+                        Data Informasi Guru
+                    </div>
+                    <div class="card-body">
+                                    <table>
+                                    <tr>
+                                        <td>
+                                            <a href = "create_guru.php"><button class="btn btn-primary me-md-2" type="button">Tambah Data Guru</button></a>
+                                        </td>
+                                        <td>
+                                            <form  action="" method="post">
+                                                <input type="text" name="keyword_guru" placeholder="Ketik Nama" autocomplete="off"> 
+                                                <button class="btn btn-primary me-md-2" type="submit" name="cari_guru">Cari</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    </table>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-strpied">
+                                        <tr class="table-primary">
+                                            <th>Aksi</th>
+                                            <th>NIP</th>
+                                            <th>Nama Lengkap</th>
+                                            <th>Tanggal Lahir</th>
+                                            <th>Jenis Kelamin</th>
+                                            <th>Alamat</th>
+                                        </tr>
+                                    <?php foreach ( $guru as $show ):  ?>
+                                        <tr class="table-info">
+                                            <td>
+                                                <a href="update_guru.php?id_guru=<?= $show ["NIP"]; ?>"><button type="button" class="btn btn-success">Ubah</button></a> |
+                                                <a href="delete_guru.php?id_guru=<?= $show ["NIP"]; ?>" onclick="return confirm('Yakin Menghapus Data ?');"><button type="button" class="btn btn-danger">Hapus</button></a>
+                                            </td>
+                                            <td> <?= $show ["NIP"] ?> </td>
+                                            <td> <?= $show ["nama"] ?> </td>
+                                            <td> <?= $show ["tanggal_lahir"] ?> </td>
+                                            <td> <?= $show ["jenis_kelamin"] ?></td>
+                                            <td> <?= $show["alamat"] ?></td>
+                                        </tr>
+                                    <?php endforeach ?>
+                        </table>
+                    </div>
+        </div>
         <br>
         <br>
         <br>
